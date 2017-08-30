9ddd43a8859fdd905720750520b7040e37168faa
srcsdk <srcsdk@users.noreply.github.com>
2017-08-29 21:15:00 -0500

    count hosts found

--- discovery/scan.sh
+++ discovery/scan.sh
@@ -7,9 +7,14 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+count=0
 for i in $(seq 1 254); do
     ping -c 1 -W 1 "$1.$i" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "$1.$i is up"
+        count=$((count + 1))
     fi
 done
+
+echo ""
+echo "$count hosts found"
