2817935d0dee8206678908e5b7ca5ba0974f9418
srcsdk <srcsdk@users.noreply.github.com>
2017-09-19 17:30:00 -0500

    scan port range

--- netscan/portscan.sh
+++ netscan/portscan.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# check if a port is open on a host
+# scan a range of ports on a host
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "usage: ./portscan.sh <host> <port>"
+if [ -z "$1" ]; then
+    echo "usage: ./portscan.sh <host> [start_port] [end_port]"
     exit 1
 fi
 
 host="$1"
-port="$2"
+start=${2:-1}
+end=${3:-1024}
 
-(echo > /dev/tcp/"$host"/"$port") 2>/dev/null
-if [ $? -eq 0 ]; then
-    echo "$host:$port open"
-else
-    echo "$host:$port closed"
-fi
+echo "scanning $host ports $start-$end..."
+echo ""
+
+for port in $(seq "$start" "$end"); do
+    (echo > /dev/tcp/"$host"/"$port") 2>/dev/null
+    if [ $? -eq 0 ]; then
+        echo "$port open"
+    fi
+done
