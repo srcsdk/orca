619e1edc83a5a575da6c55df7e868577b45ca727
srcsdk <srcsdk@users.noreply.github.com>
2017-09-30 15:20:00 -0500

    auto detect subnet from default route

--- discovery/scan.sh
+++ discovery/scan.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 # ping sweep with mac address lookup
 
-if [ -z "$1" ]; then
-    echo "usage: ./scan.sh <base_ip>"
-    echo "example: ./scan.sh 192.168.1"
-    exit 1
+show_usage() {
+    echo "usage: ./scan.sh [subnet]"
+    echo "if no subnet given, detects from default interface"
+}
+
+get_subnet() {
+    ip route | grep default | head -1 | awk '{print $3}' | sed 's/\.[0-9]*$//';
+}
+
+subnet="$1"
+if [ -z "$subnet" ]; then
+    subnet=$(get_subnet)
+    if [ -z "$subnet" ]; then
+        echo "could not detect subnet"
+        show_usage
+        exit 1
+    fi
+    echo "detected subnet: $subnet.0/24"
 fi
 
+echo ""
 count=0
 for i in $(seq 1 254); do
-    ip="$1.$i"
+    ip="$subnet.$i"
     ping -c 1 -W 1 "$ip" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         mac=$(arp -n "$ip" 2>/dev/null | grep "$ip" | awk '{print $3}')
         if [ -n "$mac" ] && [ "$mac" != "(incomplete)" ]; then
-            echo "$ip  $mac"
+            printf "%-16s %s\n" "$ip" "$mac"
         else
-            echo "$ip  (no mac)"
+            printf "%-16s %s\n" "$ip" "(unknown)"
         fi
         count=$((count + 1))
     fi
 done
 
 echo ""
-echo "$count hosts found"
+echo "$count hosts found on $subnet.0/24"
