76530f83cc80ba464854cced8fb4a3c6232ab3de
srcsdk <srcsdk@users.noreply.github.com>
2017-10-22 20:05:00 -0500

    add file output option

--- discovery/scan.sh
+++ discovery/scan.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-# ping sweep with mac address lookup
+# network host discovery via ping sweep
 
 show_usage() {
-    echo "usage: ./scan.sh [subnet]"
+    echo "usage: ./scan.sh [-o outfile] [subnet]"
     echo "if no subnet given, detects from default interface"
 }
 
 get_subnet() {
     ip route | grep default | head -1 | awk '{print $3}' | sed 's/\.[0-9]*$//';
 }
+
+outfile=""
+while getopts "o:h" opt; do
+    case $opt in
+        o) outfile="$OPTARG" ;;
+        h) show_usage; exit 0 ;;
+        *) show_usage; exit 1 ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 subnet="$1"
 if [ -z "$subnet" ]; then
@@ -18,24 +28,30 @@ if [ -z "$subnet" ]; then
         show_usage
         exit 1
     fi
-    echo "detected subnet: $subnet.0/24"
 fi
 
+echo "scanning $subnet.0/24..."
 echo ""
+
+output=""
 count=0
 for i in $(seq 1 254); do
     ip="$subnet.$i"
     ping -c 1 -W 1 "$ip" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         mac=$(arp -n "$ip" 2>/dev/null | grep "$ip" | awk '{print $3}')
-        if [ -n "$mac" ] && [ "$mac" != "(incomplete)" ]; then
-            printf "%-16s %s\n" "$ip" "$mac"
-        else
-            printf "%-16s %s\n" "$ip" "(unknown)"
-        fi
+        [ -z "$mac" ] || [ "$mac" = "(incomplete)" ] && mac="unknown"
+        line=$(printf "%-16s %s" "$ip" "$mac")
+        echo "$line"
+        output="$output$line\n"
         count=$((count + 1))
     fi
 done
 
 echo ""
-echo "$count hosts found on $subnet.0/24"
+echo "$count hosts found"
+
+if [ -n "$outfile" ]; then
+    echo -e "$output" > "$outfile"
+    echo "saved to $outfile"
+fi
