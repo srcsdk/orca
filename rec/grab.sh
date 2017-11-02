cac1b0997e6effafb279a3e150d48cba1297e860
srcsdk <srcsdk@users.noreply.github.com>
2017-11-01 20:55:00 -0500

    http probes and multiple ports

--- rec/grab.sh
+++ rec/grab.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
-# grab banner from a service
+# grab banners from services
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "usage: ./grab.sh <host> <port>"
+if [ -z "$1" ]; then
+    echo "usage: ./grab.sh <host> <port> [port] [port]..."
     exit 1
 fi
 
-echo "" | nc -w 3 "$1" "$2" 2>/dev/null | head -5
+host="$1"
+shift
+
+grab_banner() {
+    local host="$1"
+    local port="$2"
+    local banner=""
+
+    if [ "$port" -eq 80 ] || [ "$port" -eq 8080 ]; then
+        banner=$(echo -e "HEAD / HTTP/1.0\r\nHost: $host\r\n\r\n" | nc -w 3 "$host" "$port" 2>/dev/null | head -10)
+    elif [ "$port" -eq 443 ]; then
+        banner=$(echo -e "HEAD / HTTP/1.0\r\nHost: $host\r\n\r\n" | openssl s_client -connect "$host:$port" -quiet 2>/dev/null | head -10)
+    else
+        banner=$(echo "" | nc -w 3 "$host" "$port" 2>/dev/null | head -5)
+    fi
+
+    if [ -n "$banner" ]; then
+        echo "[$host:$port]"
+        echo "$banner"
+        echo ""
+    else
+        echo "[$host:$port] no banner"
+        echo ""
+    fi
+}
+
+for port in "$@"; do
+    grab_banner "$host" "$port"
+done
