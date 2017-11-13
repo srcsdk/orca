34b694e164deb60dab65624d139c004f7c597527
srcsdk <srcsdk@users.noreply.github.com>
2017-11-13 17:45:00 -0500

    parallel scanning with xargs

--- discovery/scan.sh
+++ discovery/scan.sh
@@ -2,18 +2,31 @@
 # network host discovery via ping sweep
 
 show_usage() {
-    echo "usage: ./scan.sh [-o outfile] [subnet]"
-    echo "if no subnet given, detects from default interface"
+    echo "usage: ./scan.sh [-o outfile] [-t threads] [subnet]"
 }
 
 get_subnet() {
     ip route | grep default | head -1 | awk '{print $3}' | sed 's/\.[0-9]*$//';
 }
 
+ping_host() {
+    local ip="$1"
+    ping -c 1 -W 1 "$ip" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        mac=$(arp -n "$ip" 2>/dev/null | grep "$ip" | awk '{print $3}')
+        [ -z "$mac" ] || [ "$mac" = "(incomplete)" ] && mac="unknown"
+        printf "%-16s %s\n" "$ip" "$mac"
+    fi
+}
+
+export -f ping_host
+
 outfile=""
-while getopts "o:h" opt; do
+threads=20
+while getopts "o:t:h" opt; do
     case $opt in
         o) outfile="$OPTARG" ;;
+        t) threads="$OPTARG" ;;
         h) show_usage; exit 0 ;;
         *) show_usage; exit 1 ;;
     esac
@@ -30,28 +43,17 @@ if [ -z "$subnet" ]; then
     fi
 fi
 
-echo "scanning $subnet.0/24..."
+echo "scanning $subnet.0/24 ($threads threads)..."
 echo ""
 
-output=""
-count=0
-for i in $(seq 1 254); do
-    ip="$subnet.$i"
-    ping -c 1 -W 1 "$ip" > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        mac=$(arp -n "$ip" 2>/dev/null | grep "$ip" | awk '{print $3}')
-        [ -z "$mac" ] || [ "$mac" = "(incomplete)" ] && mac="unknown"
-        line=$(printf "%-16s %s" "$ip" "$mac")
-        echo "$line"
-        output="$output$line\n"
-        count=$((count + 1))
-    fi
-done
+results=$(seq 1 254 | xargs -I{} -P "$threads" bash -c "ping_host $subnet.{}")
+echo "$results" | sort -t. -k4 -n
+count=$(echo "$results" | grep -c "\S")
 
 echo ""
 echo "$count hosts found"
 
 if [ -n "$outfile" ]; then
-    echo -e "$output" > "$outfile"
+    echo "$results" | sort -t. -k4 -n > "$outfile"
     echo "saved to $outfile"
 fi
