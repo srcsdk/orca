a1f82859c9d16ea451522aebbc69e1e8ef672777
srcsdk <srcsdk@users.noreply.github.com>
2017-12-04 16:10:00 -0500

    read ports from netscan output file

--- rec/grab.sh
+++ rec/grab.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
-# grab banners from services
+# service banner grabber
 
-if [ -z "$1" ]; then
-    echo "usage: ./grab.sh <host> <port> [port] [port]..."
-    exit 1
-fi
-
-host="$1"
-shift
+show_usage() {
+    echo "usage: ./grab.sh <host> <port> [port]..."
+    echo "       ./grab.sh -f <netscan_output> <host>"
+}
 
 grab_banner() {
     local host="$1"
     local port="$2"
     local banner=""
 
-    if [ "$port" -eq 80 ] || [ "$port" -eq 8080 ]; then
-        banner=$(echo -e "HEAD / HTTP/1.0\r\nHost: $host\r\n\r\n" | nc -w 3 "$host" "$port" 2>/dev/null | head -10)
-    elif [ "$port" -eq 443 ]; then
-        banner=$(echo -e "HEAD / HTTP/1.0\r\nHost: $host\r\n\r\n" | openssl s_client -connect "$host:$port" -quiet 2>/dev/null | head -10)
-    else
-        banner=$(echo "" | nc -w 3 "$host" "$port" 2>/dev/null | head -5)
-    fi
+    case "$port" in
+        80|8080|8000|8888)
+            banner=$(echo -e "HEAD / HTTP/1.0\r\nHost: $host\r\n\r\n" | nc -w 3 "$host" "$port" 2>/dev/null | head -10)
+            ;;
+        443|8443)
+            banner=$(echo -e "HEAD / HTTP/1.0\r\nHost: $host\r\n\r\n" | timeout 5 openssl s_client -connect "$host:$port" -quiet 2>/dev/null | head -10)
+            ;;
+        25|587)
+            banner=$(nc -w 3 "$host" "$port" 2>/dev/null | head -3)
+            ;;
+        *)
+            banner=$(echo "" | nc -w 3 "$host" "$port" 2>/dev/null | head -5)
+            ;;
+    esac
 
     if [ -n "$banner" ]; then
         echo "[$host:$port]"
         echo "$banner"
-        echo ""
     else
         echo "[$host:$port] no banner"
-        echo ""
     fi
+    echo ""
 }
 
-for port in "$@"; do
-    grab_banner "$host" "$port"
+from_file=""
+while getopts "f:h" opt; do
+    case $opt in
+        f) from_file="$OPTARG" ;;
+        h) show_usage; exit 0 ;;
+        *) show_usage; exit 1 ;;
+    esac
 done
+shift $((OPTIND - 1))
+
+host="$1"
+[ -z "$host" ] && { show_usage; exit 1; }
+shift
+
+if [ -n "$from_file" ]; then
+    ports=$(grep "open" "$from_file" | awk -F/ '{print $1}' | tr -d ' ')
+    for port in $ports; do
+        grab_banner "$host" "$port"
+    done
+else
+    for port in "$@"; do
+        grab_banner "$host" "$port"
+    done
+fi
