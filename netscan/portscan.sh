5efee838e6abb7975d9b78451ac4594fd76f4f26
srcsdk <srcsdk@users.noreply.github.com>
2018-02-02 20:20:00 -0500

    udp scan support

--- netscan/portscan.sh
+++ netscan/portscan.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# tcp port scanner
+# tcp/udp port scanner
 
 show_usage() {
-    echo "usage: ./portscan.sh [-c] [-t threads] [-o outfile] <host> [start] [end]"
+    echo "usage: ./portscan.sh [-c] [-u] [-t threads] [-o outfile] <host> [start] [end]"
     echo "  -c  common ports only"
+    echo "  -u  udp scan (requires root, slower)"
     echo "  -t  parallel threads (default 50)"
     echo "  -o  output file"
 }
@@ -12,28 +13,36 @@ common_ports="21 22 23 25 53 80 110 111 135 139 143 443 445 993 995 1723 3306 33
 
 get_service() {
     local port="$1"
-    local svc=$(grep -w "$port/tcp" /etc/services 2>/dev/null | head -1 | awk '{print $1}')
+    local proto="$2"
+    local svc=$(grep -w "$port/$proto" /etc/services 2>/dev/null | head -1 | awk '{print $1}')
     [ -z "$svc" ] && svc="unknown"
     echo "$svc"
 }
 
-check_port() {
+check_tcp() {
     local host="$1"
     local port="$2"
     timeout 1 bash -c "echo > /dev/tcp/$host/$port" 2>/dev/null
-    if [ $? -eq 0 ]; then
-        echo "$port"
-    fi
+    [ $? -eq 0 ] && echo "$port"
 }
 
-export -f check_port
+check_udp() {
+    local host="$1"
+    local port="$2"
+    timeout 2 bash -c "echo > /dev/udp/$host/$port" 2>/dev/null
+    [ $? -eq 0 ] && echo "$port"
+}
+
+export -f check_tcp check_udp
 
 scan_common=0
+udp=0
 threads=50
 outfile=""
-while getopts "ct:o:h" opt; do
+while getopts "cut:o:h" opt; do
     case $opt in
         c) scan_common=1 ;;
+        u) udp=1 ;;
         t) threads="$OPTARG" ;;
         o) outfile="$OPTARG" ;;
         h) show_usage; exit 0 ;;
@@ -48,21 +57,28 @@ if [ -z "$host" ]; then
     exit 1
 fi
 
+proto="tcp"
+[ $udp -eq 1 ] && proto="udp"
+
 if [ $scan_common -eq 1 ]; then
-    echo "scanning $host (common ports, $threads threads)..."
+    echo "scanning $host ($proto, common ports)..."
     ports="$common_ports"
 else
     start=${2:-1}
     end=${3:-1024}
-    echo "scanning $host ports $start-$end ($threads threads)..."
+    echo "scanning $host ($proto, ports $start-$end)..."
     ports=$(seq "$start" "$end")
 fi
 
-echo ""
-printf "%-8s %-8s %s\n" "port" "state" "service"
+printf "\n%-8s %-8s %s\n" "port" "state" "service"
 echo "------------------------------"
 
-results=$(echo "$ports" | tr ' ' '\n' | xargs -I{} -P "$threads" bash -c "check_port $host {}")
+if [ $udp -eq 1 ]; then
+    results=$(echo "$ports" | tr ' ' '\n' | xargs -I{} -P "$threads" bash -c "check_udp $host {}")
+else
+    results=$(echo "$ports" | tr ' ' '\n' | xargs -I{} -P "$threads" bash -c "check_tcp $host {}")
+fi
+
 open_ports=$(echo "$results" | sort -n | grep -v "^$")
 
 output=""
@@ -70,8 +86,8 @@ if [ -z "$open_ports" ]; then
     echo "no open ports found"
 else
     while read -r p; do
-        svc=$(get_service "$p")
-        line=$(printf "%-8s %-8s %s" "$p/tcp" "open" "$svc")
+        svc=$(get_service "$p" "$proto")
+        line=$(printf "%-8s %-8s %s" "$p/$proto" "open" "$svc")
         echo "$line"
         output="$output$line\n"
     done <<< "$open_ports"
