063a2c92fc52b6e1171d68b07724d5dab981645c
srcsdk <srcsdk@users.noreply.github.com>
2018-03-05 19:55:00 -0500

    cidr input and better output formatting

--- discovery/scan.sh
+++ discovery/scan.sh
@@ -2,7 +2,8 @@
 # network host discovery - ping sweep and arp scan modes
 
 show_usage() {
-    echo "usage: ./scan.sh [-o outfile] [-t threads] [-m mode] [subnet]"
+    echo "usage: ./scan.sh [-o outfile] [-t threads] [-m mode] [target]"
+    echo "target: subnet base (192.168.1) or cidr (192.168.1.0/24)"
     echo "modes: ping (default), arp"
 }
 
@@ -12,6 +13,17 @@ get_subnet() {
 
 get_interface() {
     ip route | grep default | head -1 | awk '{print $5}'
+}
+
+parse_cidr() {
+    local cidr="$1"
+    local base=$(echo "$cidr" | cut -d/ -f1 | sed 's/\.[0-9]*$//')
+    local mask=$(echo "$cidr" | cut -d/ -f2)
+    if [ "$mask" -ne 24 ] 2>/dev/null; then
+        echo "only /24 supported for now" >&2
+        exit 1
+    fi
+    echo "$base"
 }
 
 ping_host() {
@@ -32,8 +44,8 @@ arp_scan() {
     local subnet="$1"
     local iface=$(get_interface)
     if ! command -v arping &>/dev/null; then
-        echo "arping not found, falling back to ping mode"
-        ping_scan "$subnet"
+        echo "arping not found, falling back to ping" >&2
+        seq 1 254 | xargs -I{} -P "$threads" bash -c "ping_host $subnet.{}"
         return
     fi
     for i in $(seq 1 254); do
@@ -62,17 +74,22 @@ while getopts "o:t:m:h" opt; do
 done
 shift $((OPTIND - 1))
 
-subnet="$1"
-if [ -z "$subnet" ]; then
+target="$1"
+if [ -z "$target" ]; then
     subnet=$(get_subnet)
-    if [ -z "$subnet" ]; then
-        echo "could not detect subnet"
-        show_usage
-        exit 1
-    fi
+elif echo "$target" | grep -q "/"; then
+    subnet=$(parse_cidr "$target")
+else
+    subnet="$target"
 fi
 
-echo "scanning $subnet.0/24 (mode: $mode)..."
+if [ -z "$subnet" ]; then
+    echo "could not determine target subnet"
+    show_usage
+    exit 1
+fi
+
+echo "scanning $subnet.0/24 (mode: $mode, threads: $threads)..."
 printf "\n%-16s %-18s %s\n" "ip" "mac" "hostname"
 echo "------------------------------------------------"
 
@@ -89,6 +106,10 @@ echo ""
 echo "$count hosts found"
 
 if [ -n "$outfile" ]; then
-    echo "$results" | sort -t. -k4 -n > "$outfile"
+    printf "%-16s %-18s %s\n" "ip" "mac" "hostname" > "$outfile"
+    echo "------------------------------------------------" >> "$outfile"
+    echo "$results" | sort -t. -k4 -n >> "$outfile"
+    echo "" >> "$outfile"
+    echo "$count hosts found" >> "$outfile"
     echo "saved to $outfile"
 fi
