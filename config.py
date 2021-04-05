5f85b557c96234aab87cff5efbe7ef8e62ef8c44
srcsdk <srcsdk@users.noreply.github.com>
2021-04-05 10:00:00 -0500

    add config and utility modules for unified platform

---
+++ config.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+"""unified configuration management for orcasec platform"""
+
+import json
+import os
+import platform
+import sys
+
+
+PLATFORM = platform.system().lower()
+
+DEFAULT_CONFIG = {
+    "platform": PLATFORM,
+    "output_format": "table",
+    "output_dir": "",
+    "log_level": "info",
+    "log_file": "",
+    "max_threads": 50,
+    "timeout": 5,
+    "interface": "",
+    "modules": {
+        "discovery": {"enabled": True, "timeout": 2, "threads": 100},
+        "netscan": {"enabled": True, "timeout": 1, "threads": 200},
+        "target": {"enabled": True, "timeout": 5},
+        "spider": {"enabled": True, "depth": 3, "timeout": 10},
+        "detect": {"enabled": True, "interval": 5},
+        "watch": {"enabled": True, "interval": 10},
+        "flow": {"enabled": True, "duration": 60},
+        "rec": {"enabled": True, "timeout": 3},
+        "nvd": {"enabled": True, "api_key": ""},
+        "zone": {"enabled": True},
+        "poison": {"enabled": False},
+        "logma": {"enabled": True, "watch_paths": []},
+        "patch": {"enabled": True},
+        "gnore": {"enabled": True, "timeout": 5},
+        "icu": {"enabled": True, "count": 100},
+        "denied": {"enabled": True, "port": 8080},
+        "dnsguard": {"enabled": True, "interval": 30},
+        "containok": {"enabled": True},
+        "conductor": {"enabled": True},
+        "supertect": {"enabled": True},
+        "tapped": {"enabled": True},
+        "probaduce": {"enabled": True},
+        "prodsec": {"enabled": True},
+        "downseek": {"enabled": True},
+        "over": {"enabled": False},
+        "vaded": {"enabled": False},
+        "sike": {"enabled": False},
+        "tropy": {"enabled": True},
+        "weewoo": {"enabled": True},
+        "res": {"enabled": True},
+        "10fthigher": {"enabled": True},
+    },
+    "pipelines": {},
+}
+
+
+def _get_config_dir():
+    """get platform-appropriate config directory"""
+    if PLATFORM == "windows":
+        base = os.environ.get("APPDATA", os.path.expanduser("~"))
+        return os.path.join(base, "orcasec")
+    elif PLATFORM == "darwin":
+        return os.path.expanduser("~/Library/Application Support/orcasec")
+    return os.path.expanduser("~/.config/orcasec")
+
+
+def _get_data_dir():
+    """get platform-appropriate data directory"""
+    if PLATFORM == "windows":
+        base = os.environ.get("LOCALAPPDATA", os.path.expanduser("~"))
+        return os.path.join(base, "orcasec", "data")
+    elif PLATFORM == "darwin":
+        return os.path.expanduser("~/Library/Application Support/orcasec/data")
+    return os.path.expanduser("~/.local/share/orcasec")
+
+
+def _get_log_dir():
+    """get platform-appropriate log directory"""
+    if PLATFORM == "windows":
+        base = os.environ.get("LOCALAPPDATA", os.path.expanduser("~"))
+        return os.path.join(base, "orcasec", "logs")
+    return os.path.expanduser("~/.local/share/orcasec/logs")
+
+
+CONFIG_DIR = _get_config_dir()
+CONFIG_FILE = os.path.join(CONFIG_DIR, "orcasec.json")
+DATA_DIR = _get_data_dir()
+LOG_DIR = _get_log_dir()
+
+
+def load_config(path=None):
+    """load config from json file, merging with defaults"""
+    path = path or CONFIG_FILE
+    config = dict(DEFAULT_CONFIG)
+    if os.path.exists(path):
+        try:
+            with open(path, "r") as f:
+                user_config = json.load(f)
+            _merge(config, user_config)
+        except (json.JSONDecodeError, IOError) as e:
+            print(f"warning: failed to load config from {path}: {e}", file=sys.stderr)
+    if not config["output_dir"]:
+        config["output_dir"] = DATA_DIR
+    if not config["log_file"]:
+        config["log_file"] = os.path.join(LOG_DIR, "orcasec.log")
+    return config
+
+
+def save_config(config, path=None):
+    """save config to json file"""
+    path = path or CONFIG_FILE
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "w") as f:
+        json.dump(config, f, indent=2, sort_keys=True)
+    return path
+
+
+def _merge(base, override):
+    """recursively merge override dict into base dict"""
+    for key, value in override.items():
+        if key in base and isinstance(base[key], dict) and isinstance(value, dict):
+            _merge(base[key], value)
+        else:
+            base[key] = value
+
+
+def get_module_config(module_name, config=None):
+    """get config for a specific module"""
+    config = config or load_config()
+    return config.get("modules", {}).get(module_name, {})
+
+
+def set_module_config(module_name, settings, config=None):
+    """update config for a specific module"""
+    config = config or load_config()
+    if "modules" not in config:
+        config["modules"] = {}
+    if module_name not in config["modules"]:
+        config["modules"][module_name] = {}
+    config["modules"][module_name].update(settings)
+    save_config(config)
+    return config
+
+
+def ensure_dirs():
+    """create all required directories"""
+    for d in (CONFIG_DIR, DATA_DIR, LOG_DIR):
+        os.makedirs(d, exist_ok=True)
+
+
+def main():
+    """show current configuration"""
+    config = load_config()
+    print(json.dumps(config, indent=2))
+    print(f"\nconfig file: {CONFIG_FILE}")
+    print(f"data dir:    {DATA_DIR}")
+    print(f"log dir:     {LOG_DIR}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+"""shared utilities for orcasec platform"""
+
+import ctypes
+import logging
+import os
+import platform
+import socket
+import struct
+import sys
+
+
+PLATFORM = platform.system().lower()
+
+COLORS = {
+    "red": "\033[91m",
+    "green": "\033[92m",
+    "yellow": "\033[93m",
+    "blue": "\033[94m",
+    "magenta": "\033[95m",
+    "cyan": "\033[96m",
+    "reset": "\033[0m",
+    "bold": "\033[1m",
+}
+
+SEVERITY_COLORS = {
+    "critical": "red",
+    "high": "red",
+    "medium": "yellow",
+    "low": "cyan",
+    "info": "green",
+}
+
+
+def _supports_color():
+    """check if terminal supports ansi colors"""
+    if PLATFORM == "windows":
+        return os.environ.get("ANSICON") or "WT_SESSION" in os.environ
+    return hasattr(sys.stdout, "isatty") and sys.stdout.isatty()
+
+
+def colorize(text, color):
+    """wrap text in ansi color codes if supported"""
+    if not _supports_color() or color not in COLORS:
+        return text
+    return f"{COLORS[color]}{text}{COLORS['reset']}"
+
+
+class ColorFormatter(logging.Formatter):
+    """log formatter with colored severity levels"""
+
+    level_colors = {
+        logging.DEBUG: "cyan",
+        logging.INFO: "green",
+        logging.WARNING: "yellow",
+        logging.ERROR: "red",
+        logging.CRITICAL: "magenta",
+    }
+
+    def format(self, record):
+        msg = super().format(record)
+        color = self.level_colors.get(record.levelno, "reset")
+        level = colorize(record.levelname.ljust(8), color)
+        return msg.replace(record.levelname, level, 1)
+
+
+def setup_logging(name="orcasec", level="info", log_file=None):
+    """configure logging with colored console and optional file output"""
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, level.upper(), logging.INFO))
+    logger.handlers.clear()
+    console = logging.StreamHandler()
+    console.setFormatter(ColorFormatter("%(asctime)s %(levelname)s %(name)s: %(message)s",
+                                        datefmt="%H:%M:%S"))
+    logger.addHandler(console)
+    if log_file:
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+        fh = logging.FileHandler(log_file)
+        fh.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s"))
+        logger.addHandler(fh)
+    return logger
+
+
+def is_admin():
+    """check if running with elevated privileges"""
+    if PLATFORM == "windows":
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin() != 0
+        except (AttributeError, OSError):
+            return False
+    return os.geteuid() == 0
+
+
+def get_default_interface():
+    """get the default network interface name"""
+    try:
+        if PLATFORM == "linux":
+            with open("/proc/net/route", "r") as f:
+                for line in f.readlines()[1:]:
+                    fields = line.strip().split()
+                    if fields[1] == "00000000":
+                        return fields[0]
+        elif PLATFORM == "darwin":
+            import subprocess
+            result = subprocess.run(["route", "-n", "get", "default"],
+                                    capture_output=True, text=True, timeout=5)
+            for line in result.stdout.split("\n"):
+                if "interface:" in line:
+                    return line.split(":")[1].strip()
+    except (IOError, OSError, IndexError):
+        pass
+    return None
+
+
+def format_table(headers, rows, pad=2):
+    """format data as an aligned text table"""
+    if not rows:
+        return ""
+    widths = [len(str(h)) for h in headers]
+    for row in rows:
+        for i, cell in enumerate(row):
+            if i < len(widths):
+                widths[i] = max(widths[i], len(str(cell)))
+    sep = " " * pad
+    lines = []
+    header_line = sep.join(str(h).ljust(widths[i]) for i, h in enumerate(headers))
+    lines.append(colorize(header_line, "bold"))
+    lines.append(sep.join("-" * w for w in widths))
+    for row in rows:
+        cells = [str(row[i]).ljust(widths[i]) if i < len(row) else " " * widths[i]
+                 for i in range(len(headers))]
+        lines.append(sep.join(cells))
+    return "\n".join(lines)
+
+
+def main():
+    """show system info"""
+    print(f"platform:  {PLATFORM}")
+    print(f"admin:     {is_admin()}")
+    print(f"interface: {get_default_interface()}")
+    logger = setup_logging()
+    logger.info("utils loaded successfully")
+
+
+if __name__ == "__main__":
+    main()
