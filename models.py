e32c84b18aad5bf9534015f8a5b63842c6c80860
srcsdk <srcsdk@users.noreply.github.com>
2021-04-12 14:30:00 -0500

    add data models and output formatters

---
+++ models.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+"""data models for orcasec platform"""
+
+import json
+import time
+from dataclasses import dataclass, field, asdict
+
+
+@dataclass
+class Port:
+    """network port with service information"""
+    number: int
+    protocol: str = "tcp"
+    state: str = "unknown"
+    service: str = ""
+    banner: str = ""
+    version: str = ""
+
+    def __str__(self):
+        svc = f" ({self.service})" if self.service else ""
+        return f"{self.number}/{self.protocol} {self.state}{svc}"
+
+    def to_dict(self):
+        return asdict(self)
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**{k: v for k, v in data.items() if k in cls.__dataclass_fields__})
+
+
+@dataclass
+class Host:
+    """discovered network host"""
+    ip: str
+    mac: str = ""
+    hostname: str = ""
+    os_guess: str = ""
+    ports: list = field(default_factory=list)
+    last_seen: float = 0.0
+    alive: bool = True
+
+    def __post_init__(self):
+        if not self.last_seen:
+            self.last_seen = time.time()
+
+    def __str__(self):
+        name = self.hostname or self.ip
+        port_count = len(self.ports)
+        return f"{name} ({self.ip}) - {port_count} ports"
+
+    def open_ports(self):
+        """return list of open ports"""
+        return [p for p in self.ports if isinstance(p, Port) and p.state == "open"]
+
+    def to_dict(self):
+        d = asdict(self)
+        d["ports"] = [p.to_dict() if isinstance(p, Port) else p for p in self.ports]
+        return d
+
+    @classmethod
+    def from_dict(cls, data):
+        data = dict(data)
+        if "ports" in data:
+            data["ports"] = [Port.from_dict(p) if isinstance(p, dict) else p
+                             for p in data["ports"]]
+        return cls(**{k: v for k, v in data.items() if k in cls.__dataclass_fields__})
+
+
+@dataclass
+class Vulnerability:
+    """security vulnerability finding"""
+    cve_id: str = ""
+    severity: str = "unknown"
+    description: str = ""
+    affected_host: str = ""
+    affected_port: int = 0
+    affected_service: str = ""
+    cvss_score: float = 0.0
+    references: list = field(default_factory=list)
+    remediation: str = ""
+
+    def __str__(self):
+        return f"[{self.severity.upper()}] {self.cve_id}: {self.description[:80]}"
+
+    def to_dict(self):
+        return asdict(self)
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**{k: v for k, v in data.items() if k in cls.__dataclass_fields__})
+
+
+@dataclass
+class Alert:
+    """security alert from monitoring or detection"""
+    timestamp: float = 0.0
+    source: str = ""
+    severity: str = "info"
+    message: str = ""
+    details: dict = field(default_factory=dict)
+    acknowledged: bool = False
+
+    def __post_init__(self):
+        if not self.timestamp:
+            self.timestamp = time.time()
+
+    def __str__(self):
+        ts = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(self.timestamp))
+        return f"[{ts}] [{self.severity.upper()}] {self.source}: {self.message}"
+
+    def to_dict(self):
+        return asdict(self)
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**{k: v for k, v in data.items() if k in cls.__dataclass_fields__})
+
+
+@dataclass
+class ScanResult:
+    """complete scan result container"""
+    target: str = ""
+    scan_type: str = ""
+    start_time: float = 0.0
+    end_time: float = 0.0
+    hosts: list = field(default_factory=list)
+    vulnerabilities: list = field(default_factory=list)
+    alerts: list = field(default_factory=list)
+    metadata: dict = field(default_factory=dict)
+
+    def __post_init__(self):
+        if not self.start_time:
+            self.start_time = time.time()
+
+    def duration(self):
+        """scan duration in seconds"""
+        if self.end_time:
+            return self.end_time - self.start_time
+        return time.time() - self.start_time
+
+    def finish(self):
+        """mark scan as complete"""
+        self.end_time = time.time()
+        return self
+
+    def host_count(self):
+        return len(self.hosts)
+
+    def vuln_count(self):
+        return len(self.vulnerabilities)
+
+    def summary(self):
+        """brief text summary"""
+        dur = f"{self.duration():.1f}s"
+        return (f"scan of {self.target}: {self.host_count()} hosts, "
+                f"{self.vuln_count()} vulnerabilities in {dur}")
+
+    def to_dict(self):
+        return {
+            "target": self.target,
+            "scan_type": self.scan_type,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "duration": self.duration(),
+            "hosts": [h.to_dict() if isinstance(h, Host) else h for h in self.hosts],
+            "vulnerabilities": [v.to_dict() if isinstance(v, Vulnerability) else v
+                                for v in self.vulnerabilities],
+            "alerts": [a.to_dict() if isinstance(a, Alert) else a for a in self.alerts],
+            "metadata": self.metadata,
+        }
+
+    def to_json(self, indent=2):
+        return json.dumps(self.to_dict(), indent=indent)
+
+    @classmethod
+    def from_dict(cls, data):
+        data = dict(data)
+        if "hosts" in data:
+            data["hosts"] = [Host.from_dict(h) if isinstance(h, dict) else h
+                             for h in data["hosts"]]
+        if "vulnerabilities" in data:
+            data["vulnerabilities"] = [Vulnerability.from_dict(v) if isinstance(v, dict) else v
+                                       for v in data["vulnerabilities"]]
+        if "alerts" in data:
+            data["alerts"] = [Alert.from_dict(a) if isinstance(a, dict) else a
+                              for a in data["alerts"]]
+        valid = {k: v for k, v in data.items() if k in cls.__dataclass_fields__}
+        return cls(**valid)
+
+
+def main():
+    """demonstrate data models"""
+    host = Host(ip="192.168.1.1", hostname="gateway",
+                ports=[Port(80, state="open", service="http"),
+                       Port(443, state="open", service="https")])
+    vuln = Vulnerability(cve_id="CVE-2024-1234", severity="high",
+                         description="remote code execution via crafted request",
+                         affected_host="192.168.1.1", affected_port=80)
+    result = ScanResult(target="192.168.1.0/24", scan_type="full")
+    result.hosts.append(host)
+    result.vulnerabilities.append(vuln)
+    result.finish()
+    print(result.summary())
+    print(json.dumps(result.to_dict(), indent=2))
+
+
+if __name__ == "__main__":
+    main()
---
+++ output.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+"""output formatters for orcasec scan results"""
+
+import csv
+import io
+import json
+import os
+import sys
+import time
+
+from models import ScanResult, Host, Port, Vulnerability, Alert
+from utils import format_table, colorize, SEVERITY_COLORS
+
+
+def json_output(result, indent=2, file=None):
+    """format scan result as json"""
+    data = result.to_dict() if isinstance(result, ScanResult) else result
+    text = json.dumps(data, indent=indent, default=str)
+    if file:
+        with open(file, "w") as f:
+            f.write(text)
+        return file
+    return text
+
+
+def csv_output(result, file=None):
+    """format scan result hosts and ports as csv"""
+    buf = io.StringIO()
+    writer = csv.writer(buf)
+    writer.writerow(["ip", "hostname", "mac", "os", "port", "protocol",
+                     "state", "service", "banner"])
+    hosts = result.hosts if isinstance(result, ScanResult) else result
+    for host in hosts:
+        if isinstance(host, Host):
+            if not host.ports:
+                writer.writerow([host.ip, host.hostname, host.mac, host.os_guess,
+                                 "", "", "", "", ""])
+            for port in host.ports:
+                if isinstance(port, Port):
+                    writer.writerow([host.ip, host.hostname, host.mac, host.os_guess,
+                                     port.number, port.protocol, port.state,
+                                     port.service, port.banner])
+                else:
+                    writer.writerow([host.ip, host.hostname, host.mac, host.os_guess,
+                                     port, "", "", "", ""])
+        elif isinstance(host, dict):
+            writer.writerow([host.get("ip", ""), host.get("hostname", ""),
+                             host.get("mac", ""), host.get("os_guess", ""),
+                             "", "", "", "", ""])
+    text = buf.getvalue()
+    if file:
+        with open(file, "w") as f:
+            f.write(text)
+        return file
+    return text
+
+
+def table_output(result):
+    """format scan result as aligned text table"""
+    lines = []
+    if isinstance(result, ScanResult):
+        lines.append(colorize(f"scan: {result.target} ({result.scan_type})", "bold"))
+        lines.append(f"duration: {result.duration():.1f}s")
+        lines.append("")
+        if result.hosts:
+            lines.append(colorize("hosts", "cyan"))
+            headers = ["ip", "hostname", "mac", "os", "ports"]
+            rows = []
+            for host in result.hosts:
+                if isinstance(host, Host):
+                    open_ports = [str(p.number) for p in host.ports
+                                  if isinstance(p, Port) and p.state == "open"]
+                    rows.append([host.ip, host.hostname or "-", host.mac or "-",
+                                 host.os_guess or "-", ",".join(open_ports) or "-"])
+                elif isinstance(host, dict):
+                    rows.append([host.get("ip", ""), host.get("hostname", "-"),
+                                 host.get("mac", "-"), host.get("os_guess", "-"), "-"])
+            lines.append(format_table(headers, rows))
+            lines.append("")
+        if result.vulnerabilities:
+            lines.append(colorize("vulnerabilities", "red"))
+            headers = ["severity", "cve", "host", "port", "description"]
+            rows = []
+            for vuln in result.vulnerabilities:
+                if isinstance(vuln, Vulnerability):
+                    sev = vuln.severity.upper()
+                    color = SEVERITY_COLORS.get(vuln.severity.lower(), "reset")
+                    sev_colored = colorize(sev, color)
+                    rows.append([sev_colored, vuln.cve_id, vuln.affected_host,
+                                 vuln.affected_port or "-", vuln.description[:60]])
+                elif isinstance(vuln, dict):
+                    rows.append([vuln.get("severity", ""), vuln.get("cve_id", ""),
+                                 vuln.get("affected_host", ""), vuln.get("affected_port", ""),
+                                 vuln.get("description", "")[:60]])
+            lines.append(format_table(headers, rows))
+            lines.append("")
+        if result.alerts:
+            lines.append(colorize("alerts", "yellow"))
+            headers = ["time", "severity", "source", "message"]
+            rows = []
+            for alert in result.alerts:
+                if isinstance(alert, Alert):
+                    ts = time.strftime("%H:%M:%S", time.localtime(alert.timestamp))
+                    rows.append([ts, alert.severity.upper(), alert.source,
+                                 alert.message[:60]])
+            lines.append(format_table(headers, rows))
+            lines.append("")
+        lines.append(result.summary())
+    return "\n".join(lines)
+
+
+def html_report(result, file=None, title="orcasec scan report"):
+    """generate html report from scan result"""
+    data = result.to_dict() if isinstance(result, ScanResult) else result
+    ts = time.strftime("%Y-%m-%d %H:%M:%S")
+    html = [
+        "<!doctype html>",
+        "<html><head>",
+        f"<title>{title}</title>",
+        "<style>",
+        "body { font-family: monospace; margin: 20px; background: #1a1a2e; color: #e0e0e0; }",
+        "h1, h2 { color: #00d4ff; }",
+        "table { border-collapse: collapse; width: 100%; margin: 10px 0; }",
+        "th { background: #16213e; color: #00d4ff; padding: 8px; text-align: left; }",
+        "td { padding: 6px 8px; border-bottom: 1px solid #333; }",
+        "tr:hover { background: #1a1a3e; }",
+        ".critical, .high { color: #ff4444; }",
+        ".medium { color: #ffaa00; }",
+        ".low { color: #00ccff; }",
+        ".info { color: #44ff44; }",
+        ".summary { background: #16213e; padding: 15px; margin: 10px 0; }",
+        "</style>",
+        "</head><body>",
+        f"<h1>{title}</h1>",
+        f"<p>generated: {ts}</p>",
+        f"<div class='summary'><p>target: {data.get('target', '')}</p>",
+        f"<p>duration: {data.get('duration', 0):.1f}s</p>",
+        f"<p>hosts: {len(data.get('hosts', []))}</p>",
+        f"<p>vulnerabilities: {len(data.get('vulnerabilities', []))}</p></div>",
+    ]
+    hosts = data.get("hosts", [])
+    if hosts:
+        html.append("<h2>hosts</h2>")
+        html.append("<table><tr><th>ip</th><th>hostname</th><th>mac</th><th>os</th><th>ports</th></tr>")
+        for h in hosts:
+            ports = h.get("ports", [])
+            port_str = ", ".join(str(p.get("number", p) if isinstance(p, dict) else p) for p in ports)
+            html.append(f"<tr><td>{h.get('ip','')}</td><td>{h.get('hostname','')}</td>"
+                        f"<td>{h.get('mac','')}</td><td>{h.get('os_guess','')}</td>"
+                        f"<td>{port_str}</td></tr>")
+        html.append("</table>")
+    vulns = data.get("vulnerabilities", [])
+    if vulns:
+        html.append("<h2>vulnerabilities</h2>")
+        html.append("<table><tr><th>severity</th><th>cve</th><th>host</th><th>description</th></tr>")
+        for v in vulns:
+            sev = v.get("severity", "").lower()
+            html.append(f"<tr><td class='{sev}'>{sev}</td><td>{v.get('cve_id','')}</td>"
+                        f"<td>{v.get('affected_host','')}</td>"
+                        f"<td>{v.get('description','')}</td></tr>")
+        html.append("</table>")
+    html.append("</body></html>")
+    text = "\n".join(html)
+    if file:
+        os.makedirs(os.path.dirname(file) if os.path.dirname(file) else ".", exist_ok=True)
+        with open(file, "w") as f:
+            f.write(text)
+        return file
+    return text
+
+
+def format_result(result, fmt="table", file=None):
+    """dispatch to appropriate formatter"""
+    formatters = {
+        "json": lambda r: json_output(r, file=file),
+        "csv": lambda r: csv_output(r, file=file),
+        "table": lambda r: table_output(r),
+        "html": lambda r: html_report(r, file=file),
+    }
+    formatter = formatters.get(fmt, formatters["table"])
+    return formatter(result)
+
+
+def main():
+    """demo output formatters"""
+    from models import Host, Port, Vulnerability
+    result = ScanResult(target="192.168.1.0/24", scan_type="demo")
+    result.hosts = [
+        Host(ip="192.168.1.1", hostname="router", mac="aa:bb:cc:dd:ee:ff",
+             ports=[Port(80, state="open", service="http"),
+                    Port(443, state="open", service="https")]),
+        Host(ip="192.168.1.100", hostname="workstation"),
+    ]
+    result.vulnerabilities = [
+        Vulnerability(cve_id="CVE-2024-1234", severity="high",
+                      description="buffer overflow in http handler",
+                      affected_host="192.168.1.1", affected_port=80),
+    ]
+    result.finish()
+    print(table_output(result))
+
+
+if __name__ == "__main__":
+    main()
